a8a4a4ec7d94f607ea3f6c99067e5ed264b98dd6
Olaf Delgado-Friedrichs <olaf.delgado@gmail.com>
2021-02-24 18:51:50 +1100

    add a colored cube as a demo scene

--- src/Main.elm
+++ src/Main.elm
@@ -3,9 +3,11 @@ module Main exposing (main)
 import Browser
 import Color
 import Html
+import Math.Matrix4 as Mat4
 import Math.Vector3 exposing (vec3)
 import View3d.Main as View3d
-import View3d.RendererCommon
+import View3d.Mesh as Mesh
+import View3d.RendererCommon exposing (..)
 
 
 main : Program Flags Model Msg
@@ -44,12 +46,74 @@ defaultOptions =
     }
 
 
+square : Mesh.Mesh Vertex
+square =
+    Mesh.IndexedTriangles
+        [ { position = vec3 0 0 0, normal = vec3 0 0 1 }
+        , { position = vec3 1 0 0, normal = vec3 0 0 1 }
+        , { position = vec3 1 1 0, normal = vec3 0 0 1 }
+        , { position = vec3 0 1 0, normal = vec3 0 0 1 }
+        ]
+        [ ( 0, 1, 2 ), ( 0, 2, 3 ) ]
+
+
+meshes : List (Mesh.Mesh Vertex)
+meshes =
+    [ square ]
+
+
+baseMaterial : Material
+baseMaterial =
+    { color = Color.red, roughness = 0.5, metallic = 0.1 }
+
+
+instances : List Instance
+instances =
+    [ { material = { baseMaterial | color = Color.red }
+      , transform = Mat4.identity
+      , idxMesh = 0
+      , idxInstance = 0
+      }
+    , { material = { baseMaterial | color = Color.green }
+      , transform = Mat4.makeRotate (pi / 2) (vec3 1 0 0)
+      , idxMesh = 0
+      , idxInstance = 0
+      }
+    , { material = { baseMaterial | color = Color.blue }
+      , transform = Mat4.makeRotate (pi / 2) (vec3 0 -1 0)
+      , idxMesh = 0
+      , idxInstance = 0
+      }
+    , { material = { baseMaterial | color = Color.rgb 0 1 1 }
+      , transform = Mat4.makeTranslate (vec3 0 0 1)
+      , idxMesh = 0
+      , idxInstance = 0
+      }
+    , { material = { baseMaterial | color = Color.rgb 1 0 1 }
+      , transform =
+            Mat4.makeRotate (pi / 2) (vec3 1 0 0)
+                |> Mat4.translate (vec3 0 0 -1)
+      , idxMesh = 0
+      , idxInstance = 0
+      }
+    , { material = { baseMaterial | color = Color.rgb 1 1 0 }
+      , transform =
+            Mat4.makeRotate (pi / 2) (vec3 0 -1 0)
+                |> Mat4.translate (vec3 0 0 -1)
+      , idxMesh = 0
+      , idxInstance = 0
+      }
+    ]
+
+
 init : Flags -> ( Model, Cmd Msg )
 init _ =
     let
         model =
             View3d.init
                 |> View3d.setSize { width = 400, height = 400 }
+                |> View3d.setScene (Just meshes) instances
+                |> View3d.encompass
     in
     ( model, Cmd.none )
 
